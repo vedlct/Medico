c8a7826a8b4b0905cf3324b4c7175d63301e2453
Farzad <farzadrahman59@gmail.com>
2019-05-22 12:52:45 +0600

    First Commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,14 @@
+<div class="sidebar-overlay" data-reff=""></div>
+<script src="{{url('public')}}/assets/js/jquery-3.2.1.min.js"></script>
+<script src="{{url('public')}}/assets/js/popper.min.js"></script>
+<script src="{{url('public')}}/assets/js/bootstrap.min.js"></script>
+<script src="{{url('public')}}/assets/js/jquery.slimscroll.js"></script>
+<script src="{{url('public')}}/assets/js/Chart.bundle.js"></script>
+<script src="{{url('public')}}/assets/js/chart.js"></script>
+<script src="{{url('public')}}/assets/js/app.js"></script>
+
+</body>
+
+
+<!-- Mirrored from dreamguys.co.in/preclinic/template/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 22 May 2019 06:19:12 GMT -->
+</html>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- Mirrored from dreamguys.co.in/preclinic/template/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 22 May 2019 06:18:52 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+    <link rel="shortcut icon" type="image/x-icon" href="{{url('public')}}/assets/img/favicon.ico">
+    <title>Medico Dental Care</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="{{url('public')}}/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="{{url('public')}}/assets/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="{{url('public')}}/assets/css/style.css">
+    <!--[if lt IE 9]>
+    <script src="{{url('public')}}/assets/js/html5shiv.min.js"></script>
+    <script src="{{url('public')}}/assets/js/respond.min.js"></script>
+    <![endif]-->
+</head>
