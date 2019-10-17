2c8fd074f8a3409323099b6b5167260ef10295cb
ekanto <umarekanto@gmail.com>
2019-10-17 18:45:42 +0600

    .

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -1,94 +1,130 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use App\Appointment;
 use App\Doctor;
 use App\Patient;
 use App\WorkingHour;
 use Carbon\Carbon;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\DB;
 use Yajra\DataTables\DataTables;
+
 class AppointmentController extends Controller
 {
-    public function newPatient() {
+    public function newPatient()
+    {
 //        $doctors = Doctor::select('doctorId', 'firstName', 'lastName')->get();
-        $doctors = WorkingHour::select('fkdoctorId','doctorId','firstName','lastName')->leftjoin('doctor','fkdoctorId','doctorId')->get();
-        $patients =  Patient::get();
+        $doctors = WorkingHour::select('fkdoctorId', 'doctorId', 'firstName', 'lastName')->leftjoin('doctor', 'fkdoctorId', 'doctorId')->get();
+        $patients = Patient::get();
 
-        return view ('appointment.new_patient', compact('doctors','patients'));
+        return view('appointment.new_patient', compact('doctors', 'patients'));
     }
-    public function oldPatient() {
+
+    public function oldPatient()
+    {
         $days = WorkingHour::get();
-        $doctors = WorkingHour::select('fkdoctorId','doctorId','firstName','lastName')->leftjoin('doctor','fkdoctorId','doctorId')->get();
-        return view ('appointment.old_patient', compact('doctors','days'));
+        $doctors = WorkingHour::select('fkdoctorId', 'doctorId', 'firstName', 'lastName')->leftjoin('doctor', 'fkdoctorId', 'doctorId')->get();
+        return view('appointment.old_patient', compact('doctors', 'days'));
     }
+
     public function insert(Request $r)
     {
 
+        $checkday = WorkingHour::where('fkdoctorId', $r->doctorId)
+            ->where('day', date('l', strtotime($r->day)))->get();
 
 
-        $appointment = new Appointment();
-        $appointment->phone = $r->phone;
-        $appointment->age = $r->age;
-        $appointment->email = $r->email;
-        $appointment->gender = $r->gender;
-        $appointment->fkpatientId = $r->patientId;
-        $appointment->fkdoctorId = $r->doctorId;
-        $appointment->address = $r->address;
-        $appointment->day = date('l',strtotime($r->day));
-        $appointment->status = $r->status;
+        if (count($checkday) < 1) {
+            Session::flash('message', 'Doctor not available this day!');
+            Session::flash('alert-class', 'alert-danger');
+
+            return back();
+
+        } else {
+
+            $appointment = new Appointment();
+            $appointment->phone = $r->phone;
+            $appointment->age = $r->age;
+            $appointment->email = $r->email;
+            $appointment->gender = $r->gender;
+            $appointment->fkpatientId = $r->patientId;
+            $appointment->fkdoctorId = $r->doctorId;
+            $appointment->address = $r->address;
+            $appointment->day = date('l', strtotime($r->day));
+            $appointment->status = $r->status;
 
 //        $appointment->phone = $r->phone;
-        $appointment->save();
-        Session::flash('message', 'Appointment Created!');
-        Session::flash('alert-class', 'alert-success');
-        return redirect()->route('appointment');
+            $appointment->save();
+            Session::flash('message', 'Appointment Created!');
+            Session::flash('alert-class', 'alert-success');
+            return redirect()->route('appointment');
+
+
+        }
+
+
     }
-    public function showAppointment() {
-        $appointmentInfo = Appointment::select(DB::raw("concat(`patient`.`firstName`, ' ' , `patient`.`lastName`) as patientname") ,DB::raw("concat(`doctor`.`firstName`, ' ' , `doctor`.`lastName`) as doctorname") , DB::raw("CASE WHEN patient.gender = 1 THEN 'Male' WHEN patient.gender = 2 THEN 'Female' END  AS gender"), 'fkdoctorId','fkpatientId','patient.age','patient.email','patient.phone','patient.address','appointment.day','doctor.status')
-            ->leftjoin('doctor','fkdoctorId','doctorId')
-            ->leftjoin('patient','fkpatientId','patientId')->get();
+
+    public function showAppointment()
+    {
+        $appointmentInfo = Appointment::select(DB::raw("concat(`patient`.`firstName`, ' ' , `patient`.`lastName`) as patientname"), DB::raw("concat(`doctor`.`firstName`, ' ' , `doctor`.`lastName`) as doctorname"), DB::raw("CASE WHEN patient.gender = 1 THEN 'Male' WHEN patient.gender = 2 THEN 'Female' END  AS gender"), 'fkdoctorId', 'fkpatientId', 'patient.age', 'appointment.email', 'patient.phone', 'patient.address', 'appointment.day', 'doctor.status')
+            ->leftjoin('doctor', 'fkdoctorId', 'doctorId')
+            ->leftjoin('patient', 'fkpatientId', 'patientId')->get();
         $datatables = Datatables::of($appointmentInfo);
         return $datatables->make(true);
     }
-    public function index(){
-        return view ('appointment.index');
+
+    public function index()
+    {
+        return view('appointment.index');
     }
-    public function add(){
+
+    public function add()
+    {
         $doctors = Doctor::get();
-        $patients= Patient::get();
-        return view ('appointment.add', compact('doctors','patients'));
+        $patients = Patient::get();
+        return view('appointment.add', compact('doctors', 'patients'));
     }
-    public function checkoldpatient (Request $r){
+
+    public function checkoldpatient(Request $r)
+    {
         $patient = Patient::where('phone', $r->phone)->first();
         return $patient;
 //        return view('appointment.add', compact('patient'));
         $appointment = Patient::where('phone', $r->phone)->first();
         return $appointment;
     }
-    public function checkpatient (Request $r){
+
+    public function checkpatient(Request $r)
+    {
         $appointment = Patient::where('patient', $r->patient)->first();
         return $appointment;
     }
-    public function checkappointmenttime(Request $r){
+
+    public function checkappointmenttime(Request $r)
+    {
         $time = date('H:i ', strtotime($r->time));
         $doctorId = $r->doctorId;
         $appointmenttime = Appointment::
-        leftJoin('working_hour','working_hour.fkdoctorId','appointment.fkdoctorId')
+        leftJoin('working_hour', 'working_hour.fkdoctorId', 'appointment.fkdoctorId')
             ->where('working_hour.fkdoctorId', $doctorId)
             ->where('appointment.fkdoctorId', $doctorId)
             ->where('appointment_time', $time)
-            ->where(function ($query) use($time){
+            ->where(function ($query) use ($time) {
                 $query->where('start_time', '<=', $time)
                     ->Where('end_time', '>=', $time);
             })
             ->first();
         return $appointmenttime;
     }
+
     public function deleteAppointment(Request $request)
     {
-        $appointment=WorkingHour::findOrFail($request->id);
+        $appointment = WorkingHour::findOrFail($request->id);
         $appointment->delete();
     }
 }
